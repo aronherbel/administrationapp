4806457addec698e70bdffd4734347c188bf02b5
Aron Herbel <aron.herbel@ksb-sg.ch>
2024-03-21 00:15:20 +0100

    update

--- test/berechneBilanz.test.js
+++ test/berechneBilanz.test.js
@@ -3,8 +3,8 @@ const berechneBilanz = require('../server');
 const Rechnung = {
     aggregate: jest.fn().mockImplementation(async () => {
         return [
-            { _id: null, total: 100 }, // Beispielwert für Soll
-            { _id: null, total: 50 },  // Beispielwert für Haben
+            { _id: null, total: 100 },
+            { _id: null, total: 50 },
         ];
     })
 };
@@ -12,15 +12,15 @@ const Rechnung = {
 
 describe("Berechnung der Bilanz", () => {
     test("Berechnung mit positiven Werten", async () => {
-        // Aufruf der Funktion mit dem gemockten Rechnung-Modell
+        
         const bilanz = await berechneBilanz(Rechnung);
 
-        // Erwartete Ergebnisse
+        
         const expectedSoll = 100;
         const expectedHaben = 50;
         const expectedDifferenz = 50;
 
-        // Überprüfung der Ergebnisse
+        
         expect(bilanz.Soll).toBe(expectedSoll);
         expect(bilanz.Haben).toBe(expectedHaben);
         expect(bilanz.kasseBankDiffernz).toBe(expectedDifferenz);
